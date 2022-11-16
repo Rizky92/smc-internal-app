cdeaa90d15a60e2bb39b8a64966aa974a6ec523c
Rizky92 <myquinn489@gmail.com>
2022-11-16 16:33:28 +0800

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,28 +11,6 @@
 </head>
 
 <body>
-    <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-stone-50">
-  <body class="h-full">
-  ```
--->
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
