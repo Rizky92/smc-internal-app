5f3242d2f6ff5d61da10de61de37f96ae670f00d
Rizky92 <myquinn489@gmail.com>
2022-12-29 17:03:26 +0800

    Update

--- app/Http/Livewire/BaseComponent.php
+++ app/Http/Livewire/BaseComponent.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use App\Support\Traits\Livewire\FlashComponent;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 abstract class BaseComponent extends Component
 {
+    use WithPagination, FlashComponent;
+
+    public $cari;
+
+    public $perpage;
+
+    public $periodeAwal;
+
+    public $periodeAkhir;
+
+    protected function queryString()
+    {
+        return [
+            'cari' => [
+                'except' => '',
+            ],
+            'perpage' => [
+                'except' => 25,
+            ],
+            'periodeAwal' => [
+                'except' => now()->startOfMonth()->format('Y-m-d'),
+                'as' => 'periode_awal',
+            ],
+            'periodeAkhir' => [
+                'except' => now()->endOfMonth()->format('Y-m-d'),
+                'as' => 'periode_akhir',
+            ],
+        ];
+    }
+
     abstract public function render();
+
+    abstract public function defaultPropertyValues(): array;
 }
