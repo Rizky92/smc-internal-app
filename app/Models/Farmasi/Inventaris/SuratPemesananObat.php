d18aefbfb4b45b98864072340e5f703fd2caf492
Rizky92 <myquinn489@gmail.com>
2022-12-29 10:09:38 +0800

    Update

--- app/Models/Farmasi/Inventaris/SuratPemesananObat.php
+++ app/Models/Farmasi/Inventaris/SuratPemesananObat.php
@@ -68,7 +68,8 @@ class SuratPemesananObat extends Model
             })
             ->whereBetween('surat_pemesanan_medis.tanggal', [$periodeAwal, $periodeAkhir])
             ->when($hanyaTampilkanYangBerbeda, function (Builder $query) {
-                return $query->where(DB::raw('(detail_surat_pemesanan_medis.jumlah2 - pemesanan_datang.jumlah)'), '!=', 0);
+                return $query->where(DB::raw('(detail_surat_pemesanan_medis.jumlah2 - pemesanan_datang.jumlah)'), '!=', 0)
+                    ->orWhere(DB::raw("ifnull((detail_surat_pemesanan_medis.jumlah2 - pemesanan_datang.jumlah), 'Barang belum datang')"), 'Barang belum datang');
             })
             ->when(!empty($cari), function (Builder $query) use ($cari) {
                 return $query->where(function (Builder $query) use ($cari) {
