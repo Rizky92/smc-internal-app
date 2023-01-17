dd9cae8b15314b289dcbc50bab4d33ea70ece477
Rizky92 <myquinn489@gmail.com>
2023-01-17 16:49:22 +0800

    Update

---
+++ app/Http/Livewire/Keuangan/PiutangPasien.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Keuangan;
+
+use Livewire\Component;
+
+class PiutangPasien extends Component
+{
+    public function render()
+    {
+        return view('livewire.keuangan.piutang-pasien');
+    }
+}
---
+++ app/Http/Livewire/Perawatan/LaporanPasienRanap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Perawatan;
+
+use Livewire\Component;
+
+class LaporanPasienRanap extends Component
+{
+    public function render()
+    {
+        return view('livewire.perawatan.laporan-pasien-ranap');
+    }
+}
