29c7191d23dba407cd4012344849999e95e0c9a6
Rizky92 <myquinn489@gmail.com>
2023-01-30 10:23:14 +0800

    chore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SMC Custom Report</title>
+    <title>{{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 </head>
