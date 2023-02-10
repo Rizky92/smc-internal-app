3358683afe67badd5ad4a515512e9fafc8bbf1c7
Rizky92 <myquinn489@gmail.com>
2023-02-10 11:21:30 +0800

    Tracker Menu

---
+++ app/Support/Traits/Livewire/MenuTracker.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Support\Traits\Livewire;
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Str;
+
+trait MenuTracker
+{
+    public function mountMenuTracker()
+    {
+        $this->recordVisitor();
+    }
+
+    private function currentRoute(): string
+    {
+        return Route::currentRouteName();
+    }
+
+    private function menuPath(): string
+    {
+        return Breadcrumbs::generate($this->currentRoute())
+            ->map(fn ($value) => $value->title)
+            ->join(' / ');
+    }
+
+    private function recordVisitor(): void
+    {
+        $breadcrumbs = $this->menuPath();
+        $route = $this->currentRoute();
+
+        DB::connection('mysql_smc')
+            ->table('trackermenu')
+            ->insert([
+                'waktu' => now(),
+                'breadcrumbs' => $breadcrumbs,
+                'route_name' => $route,
+                'user_id' => (string) Str::of(auth()->user()->nik)->trim(),
+                'ip_address' => request()->ip(),
+            ]);
+    }
+}
