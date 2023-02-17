614a9afb4c7870521d763e83ef9fe3e0de0e348c
Rizky92 <myquinn489@gmail.com>
2023-02-17 15:13:52 +0800

    wip

--- app/Models/Perawatan/RawatInap.php
+++ app/Models/Perawatan/RawatInap.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Perawatan;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class RawatInap extends Model
@@ -34,4 +35,45 @@ class RawatInap extends Model
         'ttl_biaya',
         'stts_pulang',
     ];
+
+    public function scopePiutangRanap(
+        Builder $query,
+        string $tglAwal = '',
+        string $tglAkhir = '',
+        string $status = 'Belum Lunas',
+        string $jenisBayar = ''
+    ): Builder {
+        if (empty($tglAwal)) {
+            $tglAwal = now()->startOfMonth()->format('Y-m-d');
+        }
+
+        if (empty($tglAkhir)) {
+            $tglAkhir = now()->endOfMonth()->format('Y-m-d');
+        }
+
+        $sqlSelect = "
+            kamar_inap.no_rawat,
+            reg_periksa.no_rkm_medis,
+            pasien.nm_pasien,
+            kamar_inap.tgl_keluar,
+            penjab.png_jawab,
+            kamar_inap.stts_pulang,
+            kamar.kd_kamar,
+            bangsal.nm_bangsal,
+            piutang_pasien.uangmuka,
+            piutang_pasien.totalpiutang
+        ";
+
+        return $query
+            ->selectRaw($sqlSelect)
+            ->join('reg_periksa', 'kamar_inap.no_rawat', '=', 'reg_periksa.no_rawat')
+            ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+            ->join('penjab', 'reg_periksa.kd_pj', '=', 'penjab.kd_pj')
+            ->join('kamar', 'kamar_inap.kd_kamar', '=', 'kamar.kd_kamar')
+            ->join('bangsal', 'kamar.kd_bangsal', '=', 'bangsal.kd_bangsal')
+            ->join('piutang_pasien', 'kamar_inap.no_rawat', '=', 'piutang_pasien.no_rawat')
+            ->whereBetween('kamar_inap.tgl_keluar', [$tglAwal, $tglAkhir])
+            ->where('piutang_pasien.status', $status)
+            ->where('reg_periksa.kd_pj', $jenisBayar);
+    }
 }
