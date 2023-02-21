84acb5b740f311b9e6de24c04a5ddda48d475139
Rizky92 <myquinn489@gmail.com>
2023-02-21 11:10:23 +0800

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,5 @@
                 <h5 class="font-weight-normal">Selamat Datang, <span class="font-weight-bold">{{ Str::of(auth()->user()->nama)->title() }}!</span></h5>
             </div>
         </div>
-        <div class="row"></div>
-        <div class="row mt-3">
-            <div class="col-12">
-                <p>Menu tersedia:</p>
-                <ul>
-                    <li>Perawatan</li>
-                    <li>Keuangan</li>
-                    <li>Farmasi</li>
-                    <li>Rekam Medis</li>
-                    <li>Logistik</li>
-                    <li>Admin</li>
-                </ul>
-            </div>
-        </div>
     </div>
-
 </x-base-layout>
