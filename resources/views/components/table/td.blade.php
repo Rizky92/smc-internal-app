1c65a0ac7d43323396c5325e596a50b6019d299b
Rizky92 <myquinn489@gmail.com>
2023-02-27 16:58:29 +0800

    wip

--- resources/views/components/table/td.blade.php
+++ resources/views/components/table/td.blade.php
@@ -8,6 +8,6 @@
         <a {{ $attributes->whereStartsWith('data-')->merge([
             'style' => 'position: absolute; left: 0; right: 0; top: 0; bottom: 0',
             'href' => '#',
-        ]) }} onclick="loadData(this)"></a>
+        ]) }} onclick="loadData(this.dataset)"></a>
     @endif
 </td>
