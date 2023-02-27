5557e07f43ae4d035a6aec90830de1625ab187da
Rizky92 <myquinn489@gmail.com>
2023-02-27 11:54:11 +0800

    wip

--- resources/views/livewire/user/siap/lihat-aktivitas.blade.php
+++ resources/views/livewire/user/siap/lihat-aktivitas.blade.php
@@ -28,11 +28,16 @@
                             <div class="timeline-item border-0 shadow-none bg-transparent" style="margin-top: -0.2rem">
                                 <div class="timeline-body mt-n1 d-flex justify-content-start align-items-start">
                                     <span class="badge badge-secondary text-xs" style="margin-inline-end: 1.25rem">{{ carbon($item->waktu)->format('H:i') }}</span>
-                                    <div class="mt-n1">
+                                    <div class="mt-n1 flex-fill">
                                         <h6 style="margin-top: 0.2rem; margin-bottom: 0.25rem">
                                             Mengunjungi <a href="{{ Route::has($item->route_name) ? route($item->route_name) : '#' }}">{{ Str::of($item->breadcrumbs)->afterLast('/')->trim() }}</a>
                                         </h6>
-                                        <p class="m-0">{{ $item->breadcrumbs }}</p>
+                                        <div class="d-flex w-100 justify-content-start align-items-baseline">
+                                            <p class="m-0 d-block">{{ $item->breadcrumbs }}</p>
+                                            <div class="bg-light border rounded px-2 ml-auto" style="padding-top: 0.0625rem; padding-bottom: 0.0625rem">
+                                                <span class="text-sm font-weight-medium text-monospace">{{ $item->ip_address }}</span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
