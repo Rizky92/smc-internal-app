e5be5819f74a48feeccfe33cff290158319176b2
Rizky92 <myquinn489@gmail.com>
2023-03-08 09:10:09 +0800

    Tambah property `delay` di component loading

--- resources/views/components/card/loading.blade.php
+++ resources/views/components/card/loading.blade.php
@@ -1,7 +1,10 @@
-@props(['target' => null])
+@props([
+    'target' => null,
+    'delay' => 'longest'
+])
 
-<div wire:loading.delay.longest.class="overlay light" {{ $target ? "wire:target=\"{$target}\"" : null }}>
-    <div class="d-none justify-content-center align-items-center" wire:loading.delay.longest.class="d-flex flex-column" wire:loading.delay.longest.class.remove="d-none" {{ $target ? "wire:target=\"{$target}\"" : null }}>
+<div wire:loading.delay.{{ $delay }}.class="overlay light" {{ $target ? "wire:target=\"{$target}\"" : null }}>
+    <div class="d-none justify-content-center align-items-center" wire:loading.delay.{{ $delay }}.class="d-flex flex-column" wire:loading.delay.{{ $delay }}.class.remove="d-none" {{ $target ? "wire:target=\"{$target}\"" : null }}>
         <i class="fas fa-spinner fa-3x fa-spin"></i>
         <p class="mt-3 text-lg">Tunggu sebentar...</p>
     </div>
