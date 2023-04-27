1369323c68cdae9a464e561016f59a2b80d93741
Rizky92 <myquinn489@gmail.com>
2023-04-27 15:53:14 +0800

    Fix pencarian

--- app/Models/Aplikasi/User.php
+++ app/Models/Aplikasi/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
     protected $searchColumns = [
         'pegawai.nik',
         'pegawai.nama',
-        'jabatan.nm_jbtn',
+        'ifnull(jabatan.nm_jbtn, "")',
     ];
 
     protected $casts = [
