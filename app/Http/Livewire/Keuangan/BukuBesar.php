d96b2f080528d8afa3ee74aa60aa63756cc765ef
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2023-09-25 13:56:44 +0800

    Formatting

--- app/Http/Livewire/Keuangan/BukuBesar.php
+++ app/Http/Livewire/Keuangan/BukuBesar.php
@@ -50,24 +50,24 @@ class BukuBesar extends Component
         return $this->isDeferred
             ? []
             : Jurnal::query()
-            ->bukuBesar($this->tglAwal, $this->tglAkhir, $this->kodeRekening)
-            ->with('pengeluaranHarian')
-            ->search($this->cari, [
-                'jurnal.tgl_jurnal',
-                'jurnal.jam_jurnal',
-                'jurnal.no_jurnal',
-                'jurnal.no_bukti',
-                'jurnal.keterangan',
-                'detailjurnal.kd_rek',
-                'rekening.nm_rek',
-                'detailjurnal.debet',
-                'detailjurnal.kredit',
-            ])
-            ->sortWithColumns($this->sortColumns, [], [
-                'tgl_jurnal' => 'asc',
-                'jam_jurnal' => 'asc',
-            ])
-            ->paginate($this->perpage);
+                ->bukuBesar($this->tglAwal, $this->tglAkhir, $this->kodeRekening)
+                ->with('pengeluaranHarian')
+                ->search($this->cari, [
+                    'jurnal.tgl_jurnal',
+                    'jurnal.jam_jurnal',
+                    'jurnal.no_jurnal',
+                    'jurnal.no_bukti',
+                    'jurnal.keterangan',
+                    'detailjurnal.kd_rek',
+                    'rekening.nm_rek',
+                    'detailjurnal.debet',
+                    'detailjurnal.kredit',
+                ])
+                ->sortWithColumns($this->sortColumns, [], [
+                    'tgl_jurnal' => 'asc',
+                    'jam_jurnal' => 'asc',
+                ])
+                ->paginate($this->perpage);
     }
 
     /**
@@ -78,8 +78,8 @@ class BukuBesar extends Component
         return $this->isDeferred
             ? []
             : Jurnal::query()
-            ->jumlahDebetDanKreditBukuBesar($this->tglAwal, $this->tglAkhir, $this->kodeRekening)
-            ->first();
+                ->jumlahDebetDanKreditBukuBesar($this->tglAwal, $this->tglAkhir, $this->kodeRekening)
+                ->first();
     }
 
     public function getRekeningProperty(): array
