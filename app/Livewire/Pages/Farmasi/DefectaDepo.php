c332cf7c12c23e61dafeffb48b918c4c668abb5d
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2023-10-30 17:08:31 +0800

    WIP Defecta Depo

---
+++ app/Livewire/Pages/Farmasi/DefectaDepo.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Livewire\Pages\Farmasi;
+
+use App\Livewire\Concerns\DeferredLoading;
+use App\Livewire\Concerns\ExcelExportable;
+use App\Livewire\Concerns\Filterable;
+use App\Livewire\Concerns\FlashComponent;
+use App\Livewire\Concerns\LiveTable;
+use App\Livewire\Concerns\MenuTracker;
+use App\View\Components\BaseLayout;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class DefectaDepo extends Component
+{
+    use FlashComponent, Filterable, ExcelExportable, LiveTable, MenuTracker, DeferredLoading;
+
+    /** @var string */
+    public $tglAwal;
+
+    /** @var string */
+    public $tglAkhir;
+
+    protected function queryString(): array
+    {
+        return [
+            'tglAwal'  => ['except' => now()->startOfMonth()->format('Y-m-d'), 'as' => 'tgl_awal'],
+            'tglAkhir' => ['except' => now()->endOfMonth()->format('Y-m-d'), 'as' => 'tgl_akhir'],
+        ];
+    }
+
+    public function mount(): void
+    {
+        $this->defaultValues();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.pages.farmasi.defecta-depo')
+            ->layout(BaseLayout::class, ['title' => 'DefectaDepo']);
+    }
+
+    protected function defaultValues(): void
+    {
+        $this->tglAwal = now()->startOfMonth()->format('Y-m-d');
+        $this->tglAkhir = now()->endOfMonth()->format('Y-m-d');
+    }
+
+    protected function dataPerSheet(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function columnHeaders(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function pageHeaders(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/livewire/pages/farmasi/defecta-depo.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <x-flash />
+
+    <x-card use-default-filter>
+        <x-slot name="body">
+            <x-table :sortColumns="$sortColumns" sortable zebra hover sticky nowrap>
+                <x-slot name="columns">
+                    {{-- <x-table.th name="id" title="#" /> --}}
+                </x-slot>
+                <x-slot name="body">
+                    {{-- @forelse ($this->collectionProperty as $item)
+                        <x-table.tr>
+                            <x-table.td>{{ $item->id }}</x-table.td>
+                        </x-table.tr>
+                    @empty
+                        <x-table.tr-empty colspan="1" padding />
+                    @endforelse --}}
+                </x-slot>
+            </x-table>
+        </x-slot>
+        <x-slot name="footer">
+            {{-- <x-paginator :data="$this->collectionProperty" /> --}}
+        </x-slot>
+    </x-card>
+</div>
