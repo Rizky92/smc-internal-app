e068b878e3cd71268b678ad45b50c9d2b797aaf4
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2023-11-06 11:48:08 +0800

    Matikan unique constraint

---
+++ database/migrations/2023_11_06_093755_drop_foreign_key_constraints_in_anggaran_bidang_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table('anggaran_bidang', function (Blueprint $table): void {
+            $table->dropUnique(['anggaran_id', 'bidang_id', 'tahun']);
+        });
+        
+        Schema::enableForeignKeyConstraints();
+    }
+};
