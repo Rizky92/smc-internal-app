cfc35cddea87d8ebc893e30fdedebf6e2e02dd81
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2023-11-10 15:44:59 +0800

    Fix padding ketika tidak sedang loading

--- resources/views/components/card/loading.blade.php
+++ resources/views/components/card/loading.blade.php
@@ -3,9 +3,25 @@
     'delay' => 'longest',
 ])
 
-<div wire:loading.delay.{{ $delay }}.class="overlay light align-items-start" style="padding-top: min(1rem, 10rem)" {{ $target ? "wire:target=\"{$target}\"" : null }}>
-    <div class="d-none justify-content-center align-items-center" wire:loading.delay.{{ $delay }}.class="d-flex flex-column" wire:loading.delay.{{ $delay }}.class.remove="d-none" {{ $target ? "wire:target=\"{$target}\"" : null }}>
+@once
+    @push('css')
+        <style>
+            .overlay {
+                align-items: flex-start !important;
+                padding-top: min(10rem, 2rem);
+            }
+        </style>
+    @endpush
+@endonce
+
+<div wire:loading.delay.{{ $delay }}.class="overlay light" {{ $target ? "wire:target=\"{$target}\"" : null }}>
+    <div
+        class="d-none justify-content-center align-items-center"
+        wire:loading.delay.{{ $delay }}.class="d-flex flex-column"
+        wire:loading.delay.{{ $delay }}.class.remove="d-none"
+        {{ $target ? "wire:target=\"{$target}\"" : null }}
+    >
         <i class="fas fa-spinner fa-3x fa-spin"></i>
-        <p class="mt-3 text-lg">Tunggu sebentar...</p>
+        <p class="mt-3 text-lg">Mohon tunggu...</p>
     </div>
 </div>
