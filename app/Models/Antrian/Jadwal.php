711e5dc4555572aea3d7d1e3b9b175199c3c8eef
presidenwashil <raihanpresiden2003@gmail.com>
2024-01-18 16:26:08 +0800

    Update Jadwal.php

--- app/Models/Antrian/Jadwal.php
+++ app/Models/Antrian/Jadwal.php
@@ -77,6 +77,46 @@ class Jadwal extends Model
             ->join('poliklinik', 'jadwal.kd_poli', '=', 'poliklinik.kd_poli')
             ->where('jadwal.hari_kerja', '=', strtoupper($dayOfWeekMap[date('l')]))
             ->orderByRaw("CASE WHEN poliklinik.nm_poli = 'Poli Eksekutif' THEN 1 ELSE 0 END, poliklinik.nm_poli");
-    }    
+    }
+    
+    public static function hitungTotalRegistrasi($kd_dokter, $kd_poli, $hari_kerja, $tgl_registrasi)
+    {
+        // Mendapatkan dua jadwal dengan kondisi yang diinginkan
+        $jadwal1 = self::where('kd_dokter', $kd_dokter)
+            ->where('kd_poli', $kd_poli)
+            ->where('hari_kerja', $hari_kerja)
+            ->orderBy('jam_mulai', 'asc') // Urutkan berdasarkan jam_mulai
+            ->first();
+
+        $jadwal2 = self::where('kd_dokter', $kd_dokter)
+            ->where('kd_poli', $kd_poli)
+            ->where('hari_kerja', $hari_kerja)
+            ->where('jam_mulai', '>', $jadwal1->jam_mulai) // Jadwal yang jam_mulai lebih lambat
+            ->orderBy('jam_mulai', 'asc') // Urutkan berdasarkan jam_mulai
+            ->first();
+
+        // Jika ada dua jadwal dengan kondisi tersebut
+        if ($jadwal1 && $jadwal2) {
+            // Cek apakah total_registrasi pada jadwal yang jam_mulai lebih awal telah mencapai kuota
+            $total_registrasi_jadwal1 = RegistrasiPasien::where('kd_dokter', $kd_dokter)
+                ->where('kd_poli', $kd_poli)
+                ->where('tgl_registrasi', $tgl_registrasi)
+                ->count();
+
+            if ($total_registrasi_jadwal1 < $jadwal1->kuota) {
+                // Hitung total registrasi seperti biasa untuk jadwal yang jam_mulai lebih awal
+                return $total_registrasi_jadwal1;
+            } else {
+                // Hitung total registrasi untuk jadwal yang jam_mulai lebih lambat
+                return RegistrasiPasien::where('kd_dokter', $kd_dokter)
+                    ->where('kd_poli', $kd_poli)
+                    ->where('tgl_registrasi', $tgl_registrasi)
+                    ->count();
+            }
+        }
+
+        // Jika hanya ada satu jadwal atau tidak ada jadwal yang memenuhi kondisi
+        return 0;
+    }
     
 }
