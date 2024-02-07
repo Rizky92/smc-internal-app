91d2e8f043575df87395fc1980ed493ae6378aec
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-02-07 14:45:56 +0800

    Ubah susunan tampilan

--- resources/views/livewire/pages/keuangan/account-receivable.blade.php
+++ resources/views/livewire/pages/keuangan/account-receivable.blade.php
@@ -40,27 +40,27 @@
                     @endcan
                     <x-table.th style="width: 15ch" name="no_tagihan" title="No. Tagihan" />
                     <x-table.th style="width: 15ch" name="no_rawat" title="No. Rawat" />
-                    <x-table.th style="width: 12ch" name="tgl_tagihan" title="Tgl. Tagihan" />
-                    <x-table.th style="width: 12ch" name="tgl_jatuh_tempo" title="Tgl. Jatuh Tempo" />
-                    <x-table.th style="width: 20ch" name="tgl_bayar" title="Tgl. Bayar" />
-                    <x-table.th name="no_rkm_medis" title="No RM" />
                     <x-table.th name="nm_pasien" title="Pasien" />
-                    <x-table.th name="penjab_pasien" title="Jaminan Pasien" />
-                    <x-table.th name="penjab_piutang" title="Jaminan Akun Piutang" />
-                    <x-table.th name="catatan" title="Catatan" />
-                    <x-table.th style="width: 12ch" name="status" title="Status Piutang" />
-                    <x-table.th name="nama_bayar" title="Nama Bayar" />
                     <x-table.th name="total_piutang" title="Piutang" />
                     <x-table.th name="besar_cicilan" title="Cicilan" />
                     <x-table.th name="sisa_piutang" title="Sisa" />
                     @can('keuangan.account-receivable.validasi-piutang')
                         <x-table.th name="diskon_piutang" title="Diskon (Rp)" />
                     @endcan
+                    <x-table.th name="penjab_pasien" title="Jaminan Pasien" />
+                    <x-table.th name="penjab_piutang" title="Jaminan Akun Piutang" />
+                    <x-table.th name="catatan" title="Catatan" />
+                    <x-table.th style="width: 12ch" name="status" title="Status Piutang" />
+                    <x-table.th name="nama_bayar" title="Nama Bayar" />
+                    <x-table.th name="no_rkm_medis" title="No RM" />
                     <x-table.th style="width: 20ch" title="0 - 30" />
                     <x-table.th style="width: 20ch" title="31 - 60" />
                     <x-table.th style="width: 20ch" title="61 - 90" />
                     <x-table.th style="width: 20ch" title="> 90" />
                     <x-table.th name="kd_rek_tagihan" style="width: 20ch" title="Rekening Penagihan" />
+                    <x-table.th style="width: 12ch" name="tgl_tagihan" title="Tgl. Tagihan" />
+                    <x-table.th style="width: 12ch" name="tgl_jatuh_tempo" title="Tgl. Jatuh Tempo" />
+                    <x-table.th style="width: 20ch" name="tgl_bayar" title="Tgl. Bayar" />
                 </x-slot>
                 <x-slot name="body">
                     @forelse ($this->dataAccountReceivable as $item)
@@ -70,16 +70,7 @@
                             @endcan
                             <x-table.td>{{ $item->no_tagihan }}</x-table.td>
                             <x-table.td>{{ $item->no_rawat }}</x-table.td>
-                            <x-table.td>{{ $item->tgl_tagihan }}</x-table.td>
-                            <x-table.td>{{ $item->tgl_jatuh_tempo }}</x-table.td>
-                            <x-table.td>{{ $item->tgl_bayar ?? '-' }}</x-table.td>
-                            <x-table.td>{{ $item->no_rkm_medis }}</x-table.td>
                             <x-table.td>{{ $item->nm_pasien }}</x-table.td>
-                            <x-table.td>{{ $item->penjab_pasien }}</x-table.td>
-                            <x-table.td>{{ $item->penjab_tagihan }}</x-table.td>
-                            <x-table.td>{{ $item->catatan }}</x-table.td>
-                            <x-table.td>{{ $item->status }}</x-table.td>
-                            <x-table.td>{{ $item->nama_bayar }}</x-table.td>
                             <x-table.td>{{ rp($item->total_piutang) }}</x-table.td>
                             <x-table.td>{{ rp($item->besar_cicilan) }}</x-table.td>
                             <x-table.td>{{ rp($item->sisa_piutang) }}</x-table.td>
@@ -90,11 +81,20 @@
                                     </div>
                                 </x-table.td>
                             @endcan
+                            <x-table.td>{{ $item->penjab_pasien }}</x-table.td>
+                            <x-table.td>{{ $item->penjab_tagihan }}</x-table.td>
+                            <x-table.td>{{ $item->catatan }}</x-table.td>
+                            <x-table.td>{{ $item->status }}</x-table.td>
+                            <x-table.td>{{ $item->nama_bayar }}</x-table.td>
+                            <x-table.td>{{ $item->no_rkm_medis }}</x-table.td>
                             <x-table.td>{{ $item->umur_hari <= 30 ? rp($item->sisa_piutang) : '-' }}</x-table.td>
                             <x-table.td>{{ between($item->umur_hari, 31, 60, true) ? rp($item->sisa_piutang) : '-' }}</x-table.td>
                             <x-table.td>{{ between($item->umur_hari, 61, 90, true) ? rp($item->sisa_piutang) : '-' }}</x-table.td>
                             <x-table.td>{{ $item->umur_hari > 90 ? rp($item->sisa_piutang) : '-' }}</x-table.td>
                             <x-table.td>{{ $item->kd_rek_tagihan . ' ' . $item->nama_bank }}</x-table.td>
+                            <x-table.td>{{ $item->tgl_tagihan }}</x-table.td>
+                            <x-table.td>{{ $item->tgl_jatuh_tempo }}</x-table.td>
+                            <x-table.td>{{ $item->tgl_bayar ?? '-' }}</x-table.td>
                         </x-table.tr>
                     @empty
                         <x-table.tr-empty :colspan="user()->can('keuangan.account-receivable.validasi-piutang') ? 22 : 20" padding />
@@ -102,7 +102,7 @@
                 </x-slot>
                 <x-slot name="footer">
                     <x-table.tr>
-                        <x-table.th :colspan="user()->can('keuangan.account-receivable.validasi-piutang') ? 12 : 11" />
+                        <x-table.th :colspan="user()->can('keuangan.account-receivable.validasi-piutang') ? 3 : 2" />
                         <x-table.th title="TOTAL :" />
                         <x-table.th :title="rp(optional($this->dataTotalAccountReceivable)['totalPiutang'])" />
                         <x-table.th :title="rp(optional($this->dataTotalAccountReceivable)['totalCicilan'])" />
@@ -110,6 +110,7 @@
                         @if (user()->can('keuangan.account-receivable.validasi-piutang'))
                             <x-table.th :title="rp(optional($this->dataTotalAccountReceivable)['totalDiskonPiutang'])" />
                         @endif
+                        <x-table.th colspan="6" />
                         <x-table.th :title="rp(optional(optional($this->dataTotalAccountReceivable)['totalSisaPerPeriode'])->get('periode_0_30'))" />
                         <x-table.th :title="rp(optional(optional($this->dataTotalAccountReceivable)['totalSisaPerPeriode'])->get('periode_31_60'))" />
                         <x-table.th :title="rp(optional(optional($this->dataTotalAccountReceivable)['totalSisaPerPeriode'])->get('periode_61_90'))" />
