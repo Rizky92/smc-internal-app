4263ead801d75d4e4094ca2c18f44a1234a61997
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-02-13 16:18:05 +0800

    Formatting

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Auth\LogoutOtherSessionsController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PrintLayoutController;
-use App\Livewire\Pages\Informasi;
 use App\Livewire\Pages\Antrian;
 use App\Livewire\Pages\Aplikasi;
 use App\Livewire\Pages\Farmasi;
