1c9047a2c5531b75d50ffbc4644e28a2e8082a00
presidenwashil <raihanpresiden2003@gmail.com>
2024-03-05 11:01:05 +0800

    Add scopeLaporanTindakanLabDetail method to HasilPeriksaLab model

--- app/Models/Laboratorium/HasilPeriksaLab.php
+++ app/Models/Laboratorium/HasilPeriksaLab.php
@@ -124,4 +124,66 @@ class HasilPeriksaLab extends Model
             ->leftJoin('jns_perawatan_lab', 'periksa_lab.kd_jenis_prw', '=', 'jns_perawatan_lab.kd_jenis_prw')
             ->whereBetween('periksa_lab.tgl_periksa', [$tglAwal, $tglAkhir]);
     }
+
+    public function scopeLaporanTindakanLabDetail(Builder $query, string $tglAwal = '', string $tglAkhir = ''): Builder
+    {
+        if (empty($tglAwal)) {
+            $tglAwal = now()->startOfMonth()->format('Y-m-d');
+        }
+
+        if (empty($tglAkhir)) {
+            $tglAkhir = now()->endOfMonth()->format('Y-m-d');
+        }
+
+        $sqlSelect = <<<'SQL'
+            periksa_lab.no_rawat,
+            reg_periksa.no_rkm_medis,
+            pasien.nm_pasien,
+            pasien.tgl_lahir,
+            pasien.umur,
+            pasien.jk,
+            reg_periksa.tgl_registrasi,
+            penjab.png_jawab,
+            petugas.nama nama_petugas,
+            periksa_lab.tgl_periksa,
+            periksa_lab.jam,
+            periksa_lab.dokter_perujuk,
+            jns_perawatan_lab.kd_jenis_prw,
+            jns_perawatan_lab.nm_perawatan,
+            periksa_lab.kategori,
+            periksa_lab.biaya,
+            reg_periksa.status_bayar,
+            periksa_lab.`status`,
+            periksa_lab.kd_dokter,
+            dokter.nm_dokter
+        SQL;
+
+        $this->addSearchConditions([
+            'periksa_lab.no_rawat',
+            'reg_periksa.no_rkm_medis',
+            'pasien.nm_pasien',
+            'penjab.png_jawab',
+            'petugas.nama',
+            'periksa_lab.dokter_perujuk',
+            'jns_perawatan_lab.kd_jenis_prw',
+            'jns_perawatan_lab.nm_perawatan',
+            'periksa_lab.kategori',
+            'reg_periksa.status_bayar',
+            'periksa_lab.status',
+            'periksa_lab.kd_dokter',
+            'dokter.nm_dokter',
+        ]);
+
+        return $query
+            ->selectRaw($sqlSelect)
+            ->withCasts(['biaya' => 'float'])
+            ->leftJoin('reg_periksa', 'periksa_lab.no_rawat', '=', 'reg_periksa.no_rawat')
+            ->leftJoin('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+            ->leftJoin('petugas', 'periksa_lab.nip', '=', 'petugas.nip')
+            ->leftJoin('penjab', 'reg_periksa.kd_pj', '=', 'penjab.kd_pj')
+            ->leftJoin('dokter', 'periksa_lab.kd_dokter', '=', 'dokter.kd_dokter')
+            ->leftJoin('jns_perawatan_lab', 'periksa_lab.kd_jenis_prw', '=', 'jns_perawatan_lab.kd_jenis_prw')
+            ->whereBetween('periksa_lab.tgl_periksa', [$tglAwal, $tglAkhir])
+            ->orderBy('periksa_lab.tgl_periksa');
+    }
 }
