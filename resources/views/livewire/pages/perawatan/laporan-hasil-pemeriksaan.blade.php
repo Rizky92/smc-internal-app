1005458d84ea45304f3a58d279e2d9fe64258511
presidenwashil <raihanpresiden2003@gmail.com>
2024-03-15 08:26:01 +0800

    Refactor select2 options in laporan-hasil-pemeriksaan.blade.php

--- resources/views/livewire/pages/perawatan/laporan-hasil-pemeriksaan.blade.php
+++ resources/views/livewire/pages/perawatan/laporan-hasil-pemeriksaan.blade.php
@@ -16,8 +16,9 @@
                 <x-filter.label constant-width>Instansi:</x-filter.label>
                 <x-filter.select2
                     livewire
-                    name="perusahaan"
-                    :options="$this->dataPerusahaan"
+                    name="penjamin"
+                    :options="$this->dataPenjamin"
+                    placeholder="SEMUA"
                 />
             </x-row-col-flex>
         </x-slot>
@@ -32,7 +33,6 @@
                     <x-table.th title="Agama" />
                     <x-table.th title="Tgl. Daftar" />
                     <x-table.th title="Poli" />
-                    <x-table.th title="Instansi" />
                 </x-slot>
                 <x-slot name="body">
                     @forelse ($this->dataPasienPoliMCU as $item)
@@ -45,7 +45,6 @@
                             <x-table.td>{{ $item->pasien->agama }}</x-table.td>
                             <x-table.td>{{ $item->tgl_registrasi }}</x-table.td>
                             <x-table.td>{{ $item->poliklinik->nm_poli }}</x-table.td>
-                            <x-table.td>{{ optional($item->pasien->perusahaan)->nama_perusahaan }}</x-table.td>
                         </x-table.tr>
                     @empty
                         <x-table.tr-empty colspan="9" padding />
@@ -58,39 +57,3 @@
         </x-slot>
     </x-card>
 </div>
-
-            {{-- <x-table :sortColumns="$sortColumns" sortable zebra hover sticky nowrap>
-                <x-slot name="columns">
-                    <x-table.th title="No. Rawat" />
-                    <x-table.th title="Pasien" />
-                    <x-table.th title="Tgl. Lahir" />
-                    <x-table.th title="Umur" />
-                    <x-table.th title="JK" />
-                    <x-table.th title="Tgl. Registrasi" />
-                    @foreach ($this->dataPemeriksaan as $pemeriksaan)
-                        <x-table.th title="{{ $pemeriksaan->Pemeriksaan }} {{ $pemeriksaan->nilai_rujukan }} {{ $pemeriksaan->satuan }}" />
-                    @endforeach
-                </x-slot>
-                <x-slot name="body">
-                    @forelse ($this->detailHasilPemeriksaan as $item)
-                        <x-table.tr>
-                            <x-table.td>{{ $item->no_rawat }}</x-table.td>
-                            <x-table.td>{{ $item->nm_pasien }}</x-table.td>
-                            <x-table.td>{{ $item->tgl_lahir }}</x-table.td>
-                            <x-table.td>{{ $item->umur }}</x-table.td>
-                            <x-table.td>{{ $item->jk }}</x-table.td>
-                            <x-table.td>{{ $item->tgl_registrasi }}</x-table.td>
-                            @foreach ($this->dataPemeriksaan as $pemeriksaan)
-                                <x-table.td>{{ $pemeriksaan->nilai }}</x-table.td>
-                            @endforeach
-                        </x-table.tr>
-                    @empty
-                        <x-table.tr-empty colspan="8" padding />
-                    @endforelse
-                </x-slot>
-            </x-table> --}}
-
-
-                    {{-- <x-slot name="footer">
-            <x-paginator :data="$this->detailHasilPemeriksaan" />
-        </x-slot> --}}
