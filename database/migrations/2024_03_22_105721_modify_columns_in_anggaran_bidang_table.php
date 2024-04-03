cae2a568ed02bee63bda64788066e819739fe513
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-04-03 08:41:56 +0800

    Modify columns

--- database/migrations/2024_03_22_105721_modify_columns_in_anggaran_bidang_table.php
+++ database/migrations/2024_03_22_105721_modify_columns_in_anggaran_bidang_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Keuangan\RKAT\AnggaranBidang;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +9,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::disableForeignKeyConstraints();
+        Schema::connection('mysql_smc')->disableForeignKeyConstraints();
 
-        Schema::table('anggaran_bidang', function (Blueprint $table): void {
+        AnggaranBidang::truncate();
+
+        Schema::connection('mysql_smc')->table('anggaran_bidang', function (Blueprint $table): void {
             $table->dropColumn('nama_kegiatan');
             $table->dropColumn('deskripsi');
 
             $table->unique(['anggaran_id', 'bidang_id', 'tahun']);
         });
 
-        Schema::enableForeignKeyConstraints();
+        Schema::connection('mysql_smc')->enableForeignKeyConstraints();
     }
 };
