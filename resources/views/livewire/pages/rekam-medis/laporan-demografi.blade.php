d243dd8d5c03b32010c7b11826dfbdcd9bd8841c
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-05-03 18:29:14 +0800

    wip

--- resources/views/livewire/pages/rekam-medis/laporan-demografi.blade.php
+++ resources/views/livewire/pages/rekam-medis/laporan-demografi.blade.php
@@ -9,11 +9,12 @@
                     <x-table.th name="no_rkm_medis" title="No. RM" width="70" />
                     <x-table.th name="no_rawat" title="No. Registrasi" width="150" />
                     <x-table.th name="nm_pasien" title="Nama Pasien" width="250" />
+                    <x-table.th name="tgl_lahir" title="Tgl. Lahir" width="80" />
                     <x-table.th name="alamat" title="Alamat" width="500" />
                     <x-table.th name="umur" title="Umur" width="50" />
                     <x-table.th name="jk" title="L / P" width="50" />
-                    <x-table.th name="kd_penyakit" title="ICD-10" />
-                    <x-table.th name="nm_penyakit" title="Diagnosa" />
+                    <x-table.th title="ICD-10" />
+                    <x-table.th title="Diagnosa" />
                     <x-table.th name="agama" title="Agama" width="100" />
                     <x-table.th name="pnd" title="Pendidikan" width="100" />
                     <x-table.th name="nama_bahasa" title="Bahasa" width="100" />
@@ -26,6 +27,7 @@
                             <x-table.td>{{ $pasien->no_rkm_medis }}</x-table.td>
                             <x-table.td>{{ $pasien->no_rawat }}</x-table.td>
                             <x-table.td>{{ $pasien->nm_pasien }}</x-table.td>
+                            <x-table.td>{{ $pasien->tgl_lahir }}</x-table.td>
                             <x-table.td>{{ $pasien->alamat }}</x-table.td>
                             <x-table.td>{{ sprintf('%s %s', $pasien->umurdaftar, $pasien->sttsumur) }}</x-table.td>
                             <x-table.td>{{ $pasien->jk }}</x-table.td>
