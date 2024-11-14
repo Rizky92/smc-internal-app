85342d1a00e8a61137dd84e46f5e4eac6c398a93
presidenwashil <raihanpresiden2003@gmail.com>
2024-11-14 17:02:21 +0800

    WIP

---
+++ resources/views/livewire/antrean.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <h1 class="text-center p-4">ANTREAN POLIKLINIK</h1>
+    <div class="row">
+        @foreach ($this->poli as $item)
+            <div class="col-3">
+
+                <a href="{{ route('antrean-poli', ['kd_poli' => $item->kd_poli]) }}" class="text-uppercase text-decoration-none text-white">
+                    <div class="card text-white bg-gradient-success mb-3" style="max-width: 18rem; font-size: 0.75rem;">
+                        <div class="card-header">
+                            {{ $item->nm_poli }}
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
