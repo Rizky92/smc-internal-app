6ac700c0452f099cc5fe57f875cba0f27220e076
presidenwashil <raihanpresiden2003@gmail.com>
2024-12-11 08:52:19 +0800

    Add: stok_saat_ini field to PemakaianStokFarmasi and Obat models, update view to display current stock

--- app/Models/Farmasi/Obat.php
+++ app/Models/Farmasi/Obat.php
@@ -196,6 +196,7 @@ class Obat extends Model
             databarang.nama_brng,
             kodesatuan.satuan satuan_kecil,
             kategori_barang.nama kategori,
+            (ifnull((select round(sum(gudangbarang.stok), 2) from gudangbarang where gudangbarang.kode_brng = databarang.kode_brng), 0)) stok_saat_ini,
             (
                 ifnull((select round(sum(detail_pemberian_obat.jml), 2) from detail_pemberian_obat where detail_pemberian_obat.kode_brng = databarang.kode_brng and detail_pemberian_obat.tgl_perawatan between date_sub(current_date(), interval 2 week) and current_date()), 0) + 
                 ifnull((select round(sum(detailjual.jumlah), 2) from detailjual join penjualan on detailjual.nota_jual = penjualan.nota_jual where detailjual.kode_brng = databarang.kode_brng and penjualan.tgl_jual between date_sub(current_date(), interval 2 week) and current_date()), 0) 
@@ -237,6 +238,7 @@ class Obat extends Model
         $this->addRawColumns([
             'satuan_kecil'         => 'kodesatuan.satuan',
             'kategori'             => 'kategori_barang.nama',
+            'stok_saat_ini'        => DB::raw('(ifnull((select round(sum(gudangbarang.stok), 2) from gudangbarang where gudangbarang.kode_brng = databarang.kode_brng), 0))'),
             'ke_pasien_14_hari'    => DB::raw('(ifnull((select round(sum(detail_pemberian_obat.jml), 2) from detail_pemberian_obat where detail_pemberian_obat.kode_brng = databarang.kode_brng and detail_pemberian_obat.tgl_perawatan between date_sub(current_date(), interval 2 week) and current_date()), 0) + ifnull((select round(sum(detailjual.jumlah), 2) from detailjual join penjualan on detailjual.nota_jual = penjualan.nota_jual where detailjual.kode_brng = databarang.kode_brng and penjualan.tgl_jual between date_sub(current_date(), interval 2 week) and current_date()), 0))'),
             'pemakaian_1_minggu'   => DB::raw('(ifnull((select round(sum(detail_pemberian_obat.jml), 2) from detail_pemberian_obat where detail_pemberian_obat.kode_brng = databarang.kode_brng and detail_pemberian_obat.tgl_perawatan between date_sub(current_date(), interval 1 week) and current_date()), 0) + ifnull((select round(sum(detailjual.jumlah), 2) from detailjual join penjualan on detailjual.nota_jual = penjualan.nota_jual where detailjual.kode_brng = databarang.kode_brng and penjualan.tgl_jual between date_sub(current_date(), interval 1 week) and current_date()), 0) + ifnull((select round(sum(detail_pengeluaran_obat_bhp.jumlah), 2) from detail_pengeluaran_obat_bhp join pengeluaran_obat_bhp on detail_pengeluaran_obat_bhp.no_keluar = pengeluaran_obat_bhp.no_keluar where detail_pengeluaran_obat_bhp.kode_brng = databarang.kode_brng and pengeluaran_obat_bhp.tanggal between date_sub(current_date(), interval 1 week) and current_date()), 0))'),
             'pemakaian_1_bulan'    => DB::raw('(ifnull((select round(sum(detail_pemberian_obat.jml), 2) from detail_pemberian_obat where detail_pemberian_obat.kode_brng = databarang.kode_brng and detail_pemberian_obat.tgl_perawatan between date_sub(current_date(), interval 1 month) and current_date()), 0) + ifnull((select round(sum(detailjual.jumlah), 2) from detailjual join penjualan on detailjual.nota_jual = penjualan.nota_jual where detailjual.kode_brng = databarang.kode_brng and penjualan.tgl_jual between date_sub(current_date(), interval 1 month) and current_date()), 0) + ifnull((select round(sum(detail_pengeluaran_obat_bhp.jumlah), 2) from detail_pengeluaran_obat_bhp join pengeluaran_obat_bhp on detail_pengeluaran_obat_bhp.no_keluar = pengeluaran_obat_bhp.no_keluar where detail_pengeluaran_obat_bhp.kode_brng = databarang.kode_brng and pengeluaran_obat_bhp.tanggal between date_sub(current_date(), interval 1 month) and current_date()), 0))'),
@@ -248,6 +250,7 @@ class Obat extends Model
         return $query
             ->selectRaw($sqlSelect)
             ->withCasts([
+                'stok_saat_ini'       => 'float',
                 'ke_pasien_14_hari'   => 'float',
                 'pemakaian_1_minggu'  => 'float',
                 'pemakaian_1_bulan'   => 'float',
