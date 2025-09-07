7087125fc4b7c2ce6f0f61b6e5b8c381fcbbccb4
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2025-09-08 07:31:05 +0800

    Disable translate
    Signed-off-by: Muhammad Rizky Rizaldi <myquinn489@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="google" content="notranslate" />
         <title>{{ $title ?? config('app.name') }}</title>
-
         <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('css/responsive.bootstrap4.min.css') }}" />
         <link href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet" />
