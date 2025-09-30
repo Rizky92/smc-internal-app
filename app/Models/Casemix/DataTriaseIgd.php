74cd7d53b7dc12864176a665618b1ff5a7ead633
presidenwashil <raihanpresiden2003@gmail.com>
2025-09-30 15:10:54 +0800

    feat: add No. SEP and Status columns to Laporan Triase IGD Zona Hijau

--- app/Models/Casemix/DataTriaseIgd.php
+++ app/Models/Casemix/DataTriaseIgd.php
@@ -31,11 +31,13 @@ class DataTriaseIgd extends Model
 
         $this->addSearchConditions([
             'data_triase_igd.no_rawat',
+            'bridging_sep.no_sep',
             'reg_periksa.no_rkm_medis',
             'pasien.nm_pasien',
             'penjab.png_jawab',
             'data_triase_igd.tgl_kunjungan',
             'data_triase_igd.cara_masuk',
+            'reg_periksa.status_lanjut',
             'data_triase_igd.alasan_kedatangan',
             'master_triase_macam_kasus.macam_kasus',
             'data_triase_igdsekunder.plan',
@@ -43,11 +45,13 @@ class DataTriaseIgd extends Model
 
         $sqlSelect = <<<SQL
             data_triase_igd.no_rawat no_rawat,
+            bridging_sep.no_sep no_sep,
             reg_periksa.no_rkm_medis no_rkm_medis,
             pasien.nm_pasien nm_pasien,
             penjab.png_jawab png_jawab,
             data_triase_igd.tgl_kunjungan tgl_kunjungan,
             data_triase_igd.cara_masuk cara_masuk,
+            reg_periksa.status_lanjut status_lanjut,
             data_triase_igd.alasan_kedatangan alasan_kedatangan,
             master_triase_macam_kasus.macam_kasus macam_kasus,
             data_triase_igdsekunder.plan plan
@@ -57,6 +61,7 @@ class DataTriaseIgd extends Model
             ->selectRaw($sqlSelect)
             ->join('master_triase_macam_kasus', 'data_triase_igd.kode_kasus', '=', 'master_triase_macam_kasus.kode_kasus')
             ->join('reg_periksa', 'data_triase_igd.no_rawat', '=', 'reg_periksa.no_rawat')
+            ->join('bridging_sep', 'data_triase_igd.no_rawat', '=', 'bridging_sep.no_rawat')
             ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
             ->join('penjab', 'reg_periksa.kd_pj', '=', 'penjab.kd_pj')
             ->join('data_triase_igdsekunder', 'data_triase_igd.no_rawat', '=', 'data_triase_igdsekunder.no_rawat')
