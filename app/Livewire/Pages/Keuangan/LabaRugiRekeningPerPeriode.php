7ed17e61acd4014ef75dc93632d4ab224a0a58cf
Raihan Presiden Washil <raihanpresiden2003@gmail.com>
2026-02-24 13:46:29 +0800

    wip

--- app/Livewire/Pages/Keuangan/LabaRugiRekeningPerPeriode.php
+++ app/Livewire/Pages/Keuangan/LabaRugiRekeningPerPeriode.php
@@ -8,7 +8,7 @@ use App\Livewire\Concerns\Filterable;
 use App\Livewire\Concerns\FlashComponent;
 use App\Livewire\Concerns\LiveTable;
 use App\Livewire\Concerns\MenuTracker;
-use App\Models\Keuangan\Rekening;
+use App\Models\Keuangan\Jurnal\Jurnal;
 use App\Models\RekamMedis\Penjamin;
 use App\View\Components\BaseLayout;
 use Illuminate\Support\Collection;
@@ -51,50 +51,37 @@ class LabaRugiRekeningPerPeriode extends Component
     public function getLabaRugiPerRekeningProperty(): Collection
     {
         if ($this->isDeferred) {
-            return collect(['D' => [], 'K' => []]);
+            return collect(['D' => collect(), 'K' => collect()]);
         }
 
-        $semuaRekening = Rekening::semuaRekening()
-            ->whereTipe('R')
-            ->get();
-
-        $debetKredit = Rekening::query()
-            ->hitungDebetKreditPerPeriode($this->tglAwal, $this->tglAkhir, $this->kodePenjamin)
-            ->get();
-
-        return $semuaRekening
-            ->merge($debetKredit)
-            ->map(function (Rekening $rekening): Fluent {
-                $total = 0;
-
-                $debet = $rekening->debet ?? 0;
-                $kredit = $rekening->kredit ?? 0;
-
-                if ($rekening->balance === 'K') {
-                    $total = $kredit - $debet;
-                }
-
-                if ($rekening->balance === 'D') {
-                    $total = $debet - $kredit;
-                }
-
-                return new Fluent(array_merge(
-                    $rekening->only('kd_rek', 'nm_rek', 'balance'),
-                    [
-                        'debet'  => floatval($debet),
-                        'kredit' => floatval($kredit),
-                        'total'  => floatval($total),
-                    ],
-                ));
-            })
+        $groups = Jurnal::query()->labaRugiRalan($this->tglAwal, $this->tglAkhir, $this->kodePenjamin)
+            ->unionAll(Jurnal::query()->labaRugiRanap($this->tglAwal, $this->tglAkhir, $this->kodePenjamin))
+            ->unionAll(Jurnal::query()->labaRugi($this->tglAwal, $this->tglAkhir, $this->kodePenjamin))
+            ->get()
+            ->map(fn ($item): Fluent => new Fluent([
+                'unit'         => $item->unit,
+                'nm_dokter'    => $item->nm_dokter ?: '-',
+                'kd_rek'       => $item->kd_rek,
+                'nm_rek'       => $item->nm_rek,
+                'balance'      => $item->balance,
+                'debet'        => floatval($item->debet),
+                'kredit'       => floatval($item->kredit),
+                'total'        => $item->balance === 'K'
+                    ? floatval($item->kredit - $item->debet)
+                    : floatval($item->debet - $item->kredit),
+            ]))
             ->mapToGroups(fn ($item): array => [$item->balance => $item]);
+
+        return collect(['D' => collect(), 'K' => collect()])->merge($groups);
     }
 
     public function getTotalLabaRugiPerRekeningProperty(): array
     {
-        $pendapatan = collect($this->labaRugiPerRekening->get('K'));
+        $semua = $this->labaRugiPerRekening;
 
-        $bebanDanBiaya = collect($this->labaRugiPerRekening->get('D'));
+        $pendapatan = $semua->get('K', collect());
+
+        $bebanDanBiaya = $semua->get('D', collect());
 
         $totalDebetPendapatan = $pendapatan->sum('debet');
         $totalKreditPendapatan = $pendapatan->sum('kredit');
@@ -141,15 +128,15 @@ class LabaRugiRekeningPerPeriode extends Component
         $bebanRowHeader = $this->insertExcelRow('', 'BEBAN & BIAYA');
         $empty = $this->insertExcelRow();
 
-        $pendapatan = $this->labaRugiPerRekening->get('K');
-        $beban = $this->labaRugiPerRekening->get('D');
+        $pendapatan = $this->labaRugiPerRekening->get('K', collect());
+        $beban = $this->labaRugiPerRekening->get('D', collect());
 
         $total = $this->totalLabaRugiPerRekening;
 
-        $totalPendapatanRow = $this->insertExcelRow('', 'TOTAL', '', $total['totalDebetPendapatan'], $total['totalKreditPendapatan'], $total['totalPendapatan']);
-        $totalBebanRow = $this->insertExcelRow('', 'TOTAL', '', $total['totalDebetBeban'], $total['totalKreditBeban'], $total['totalBebanDanBiaya']);
+        $totalPendapatanRow = $this->insertExcelRow('', 'TOTAL', '', '', '', $total['totalDebetPendapatan'], $total['totalKreditPendapatan'], $total['totalPendapatan']);
+        $totalBebanRow = $this->insertExcelRow('', 'TOTAL', '', '', '', $total['totalDebetBeban'], $total['totalKreditBeban'], $total['totalBebanDanBiaya']);
 
-        $pendapatanBersih = $this->insertExcelRow('', 'PENDAPATAN BERSIH', '', $total['totalPendapatan'], $total['totalBebanDanBiaya'], $total['labaRugi']);
+        $pendapatanBersih = $this->insertExcelRow('', 'PENDAPATAN BERSIH', '', '', '', $total['totalPendapatan'], $total['totalBebanDanBiaya'], $total['labaRugi']);
 
         return collect([$pendapatanRowHeader])
             ->merge($pendapatan)
@@ -160,7 +147,7 @@ class LabaRugiRekeningPerPeriode extends Component
             ->merge([$pendapatanBersih]);
     }
 
-    private function insertExcelRow(string $kd_rek = '', string $nm_rek = '', string $balance = '', string $debet = '', string $kredit = '', string $total = ''): Fluent
+    private function insertExcelRow(string $unit = '', string $nm_dokter = '', string $kd_rek = '', string $nm_rek = '', string $balance = '', string $debet = '', string $kredit = '', string $total = ''): Fluent
     {
         return new Fluent(func_get_named_args($this, 'insertExcelRow', func_get_args()));
     }
@@ -180,6 +167,8 @@ class LabaRugiRekeningPerPeriode extends Component
     protected function columnHeaders(): array
     {
         return [
+            'Unit',
+            'Dokter',
             'Kode Akun',
             'Nama Akun',
             'Jenis',
