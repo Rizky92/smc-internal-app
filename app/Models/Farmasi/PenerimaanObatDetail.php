8c2d7f514344834a9e64c72a79fdeab2e8a43333
Raihan Presiden Washil <raihanpresiden2003@gmail.com>
2026-02-27 09:42:44 +0800

    feat: add HPP Pembelian Terakhir page and related functionality

--- app/Models/Farmasi/PenerimaanObatDetail.php
+++ app/Models/Farmasi/PenerimaanObatDetail.php
@@ -3,6 +3,7 @@
 namespace App\Models\Farmasi;
 
 use App\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class PenerimaanObatDetail extends Model
 {
@@ -17,4 +18,72 @@ class PenerimaanObatDetail extends Model
     public $incrementing = false;
 
     public $timestamps = false;
+
+    public function scopeHppPembelianTerakhir(Builder $query, $kodeBangsal = ''): Builder
+    {
+        $hargaKecil = 'detailpesan.h_pesan / NULLIF(detailpesan.jumlah2 / NULLIF(detailpesan.jumlah, 0), 0)';
+        $setelahDiskon = "($hargaKecil * (1 - (detailpesan.dis / 100)))";
+        $hpp = "round(($setelahDiskon * 1.11), 2)";
+
+        $this->addSearchConditions([
+            'x.kode_brng',
+            'x.nama_brng',
+            'x.nm_bangsal',
+        ]);
+
+        return $query->fromSub(function ($sub) use ($hargaKecil, $hpp, $kodeBangsal) {
+
+            $sub->selectRaw("
+                    bangsal.nm_bangsal,
+                    detailpesan.kode_brng,
+                    databarang.nama_brng,
+                    databarang.kode_satbesar,
+                    databarang.isi,
+                    databarang.kode_sat,
+                    databarang.kapasitas,
+                    gudangbarang.stok,
+                    detailpesan.h_pesan,
+                    detailpesan.dis,
+                    round($hargaKecil, 2) as harga_satuan,
+                    $hpp as hpp
+                ")
+                ->from('detailpesan')
+                ->join('pemesanan', 'detailpesan.no_faktur', '=', 'pemesanan.no_faktur')
+                ->join('databarang', 'detailpesan.kode_brng', '=', 'databarang.kode_brng')
+                ->join('gudangbarang', 'detailpesan.kode_brng', '=', 'gudangbarang.kode_brng')
+                ->join('bangsal', 'gudangbarang.kd_bangsal', '=', 'bangsal.kd_bangsal')
+                ->joinSub(
+                    function ($query) {
+                        $query->selectRaw('kode_brng, tgl_terakhir, h_pesan_terakhir')
+                            ->fromSub(function ($inner) {
+                                $inner->selectRaw('
+                                        d2.kode_brng,
+                                        p2.tgl_pesan AS tgl_terakhir,
+                                        d2.h_pesan AS h_pesan_terakhir,
+                                        ROW_NUMBER() OVER (
+                                            PARTITION BY d2.kode_brng
+                                            ORDER BY p2.tgl_pesan DESC, d2.h_pesan DESC
+                                        ) AS rn
+                                    ')
+                                    ->from('detailpesan as d2')
+                                    ->join('pemesanan as p2', 'd2.no_faktur', '=', 'p2.no_faktur');
+                            }, 'ranked')
+                            ->where('rn', 1);
+                    },
+                    'last_order',
+                    function ($join) {
+                        $join->on('detailpesan.kode_brng', '=', 'last_order.kode_brng')
+                            ->on('pemesanan.tgl_pesan', '=', 'last_order.tgl_terakhir')
+                            ->on('detailpesan.h_pesan', '=', 'last_order.h_pesan_terakhir');
+                    }
+                )->when($kodeBangsal !== '-', function ($q) use ($kodeBangsal) {
+                    $q->where('bangsal.kd_bangsal', $kodeBangsal);
+                });
+        }, 'x')
+            ->selectRaw('
+            x.*,
+            ROUND(x.hpp * x.stok, 2) as total_nilai_stok
+        ')
+            ->orderBy('x.kode_brng', 'asc');
+    }
 }
---
+++ resources/views/livewire/pages/farmasi/hpp-pembelian-terakhir.blade.php
@@ -0,0 +1,61 @@
+<div wire:init="loadProperties">
+    <x-flash />
+
+    <x-card use-loading>
+        <x-slot name="header">
+            <x-row-col-flex>
+                <x-filter.label constant-width>Ruangan :</x-filter.label>
+                <x-filter.select2 livewire name="kodeBangsal" placeholder="-" :options="$this->bangsal" />
+                <x-filter.button-export-excel class="ml-auto" />
+            </x-row-col-flex>
+            <x-row-col-flex class="mt-2">
+                <x-filter.select-perpage :constantWidth="true" />
+                <x-filter.button-reset-filters class="ml-auto" />
+                <x-filter.search class="ml-2" />
+            </x-row-col-flex>
+        </x-slot>
+        <x-slot name="body">
+            <x-table :sortColumns="$sortColumns" sortable zebra hover sticky nowrap>
+                <x-slot name="columns">
+                    <x-table.th name="nm_bangsal" title="Ruangan" />
+                    <x-table.th name="kode_brng" title="Kode" />
+                    <x-table.th name="nama_brng" title="Nama" />
+                    <x-table.th name="kode_satbesar" title="Satuan Besar" />
+                    <x-table.th name="isi" title="Isi" />
+                    <x-table.th name="kode_sat" title="Satuan Kecil" />
+                    <x-table.th name="kapasitas" title="Kapasitas" />
+                    <x-table.th name="stok" title="Stok" />
+                    <x-table.th name="h_pesan" title="Harga Beli" />
+                    <x-table.th name="dis" title="Diskon %" />
+                    <x-table.th name="harga_satuan" title="Harga Satuan" />
+                    <x-table.th name="hpp" title="HPP" />
+                    <x-table.th name="total_nilai_stok" title="Total Nilai Stok" />
+                </x-slot>
+                <x-slot name="body">
+                    @forelse ($this->collection as $item)
+                        <x-table.tr>
+                            <x-table.td>{{ $item->nm_bangsal }}</x-table.td>
+                            <x-table.td>{{ $item->kode_brng }}</x-table.td>
+                            <x-table.td>{{ $item->nama_brng }}</x-table.td>
+                            <x-table.td>{{ $item->kode_satbesar }}</x-table.td>
+                            <x-table.td>{{ $item->isi }}</x-table.td>
+                            <x-table.td>{{ $item->kode_sat }}</x-table.td>
+                            <x-table.td>{{ $item->kapasitas }}</x-table.td>
+                            <x-table.td>{{ $item->stok }}</x-table.td>
+                            <x-table.td>{{ rp($item->h_pesan) }}</x-table.td>
+                            <x-table.td>{{ $item->dis }}%</x-table.td>
+                            <x-table.td>{{ rp($item->harga_satuan) }}</x-table.td>
+                            <x-table.td>{{ rp($item->hpp) }}</x-table.td>
+                            <x-table.td>{{ rp($item->total_nilai_stok) }}</x-table.td>
+                        </x-table.tr>
+                    @empty
+                        <x-table.tr-empty colspan="13" padding />
+                    @endforelse
+                </x-slot>
+            </x-table>
+        </x-slot>
+        <x-slot name="footer">
+            <x-paginator :data="$this->collection" />
+        </x-slot>
+    </x-card>
+</div>
