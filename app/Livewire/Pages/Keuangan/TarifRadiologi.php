d1f9fafd1408ce2385bedcd357062b3d9b73f520
Raihan Presiden Washil <raihanpresiden2003@gmail.com>
2026-05-22 13:45:53 +0800

    fix(export): add 'Kode Jenis Bayar' to export and table display

--- app/Livewire/Pages/Keuangan/TarifRadiologi.php
+++ app/Livewire/Pages/Keuangan/TarifRadiologi.php
@@ -68,6 +68,7 @@ class TarifRadiologi extends Component
                     Str::transliterate($model->kso),
                     Str::transliterate($model->menejemen),
                     Str::transliterate($model->total_byr),
+                    Str::transliterate($model->kd_pj),
                     Str::transliterate($model->png_jawab),
                     Str::transliterate($model->kelas),
                 ]),
@@ -87,6 +88,7 @@ class TarifRadiologi extends Component
             'KSO',
             'Menejemen',
             'Total Tarif',
+            'Kode Jenis Bayar',
             'Jenis Bayar',
             'Kelas',
         ];
