b5c2aaba70c1ea95682bbb2532b6919220e18817
Raihan Presiden Washil <108395093+presidenwashil@users.noreply.github.com>
2026-09-04 14:48:07 +0800

    Laporan Pemakaian Antibiotik (#250)

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -262,6 +262,11 @@ Breadcrumbs::for('admin.farmasi.laporan-pemakaian-obat-tb', function (Trail $tra
     $trail->push('Pemakaian Obat TB', route('admin.farmasi.laporan-pemakaian-obat-tb'));
 });
 
+Breadcrumbs::for('admin.farmasi.laporan-pemakaian-obat-antibiotik', function (Trail $trail): void {
+    $trail->parent('admin.farmasi');
+    $trail->push('Pemakaian Obat Antibiotik', route('admin.farmasi.laporan-pemakaian-obat-antibiotik'));
+});
+
 Breadcrumbs::for('admin.farmasi.defecta-depo', function (Trail $trail): void {
     $trail->parent('admin.farmasi');
     $trail->push('Defecta Depo', route('admin.farmasi.defecta-depo'));
